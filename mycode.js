72221bb8fa9b9f08cf73a7bb437c84ae28cf3d12
jotowibowo <66049601+jotowibowo@users.noreply.github.com>
2022-12-22 13:38:46 +0700

    Add files via upload

--- mycode.js
+++ mycode.js
@@ -1,4 +1,4 @@
 function myFunction() {
-    document.getElementById("demo").innerHTML="Berubah Jinx!!!!";
+    document.getElementById("demo").innerHTML="Berubah Jd bau!!!!";
     sessionStorage.clear();
 }
