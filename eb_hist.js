43cea02f059aa796aebedd8cfe3c9cdcb4b4a281
jotowibowo <66049601+jotowibowo@users.noreply.github.com>
2023-01-02 16:43:57 +0700

    Add files via upload

--- eb_hist.js
+++ eb_hist.js
@@ -4,6 +4,7 @@
 {"id":3,"email":"emma.wong@reqres.in","first_name":"Emma","last_name":"Wong","avatar":"https://reqres.in/img/faces/3-image.jpg"},
 {"id":4,"email":"eve.holt@reqres.in","first_name":"Eve","last_name":"Holt","avatar":"https://reqres.in/img/faces/4-image.jpg"},
 {"id":5,"email":"charles.morris@reqres.in","first_name":"Charles","last_name":"Morris","avatar":"https://reqres.in/img/faces/5-image.jpg"},
-{"id":6,"email":"tracey.ramos@reqres.in","first_name":"Tracey","last_name":"Ramos","avatar":"https://reqres.in/img/faces/6-image.jpg"}
+{"id":6,"email":"tracey.ramos@reqres.in","first_name":"Tracey","last_name":"Ramos","avatar":"https://reqres.in/img/faces/6-image.jpg"},
+{"id":7,"email":"jul.bra@reqres.in","first_name":"Jul","last_name":"Bra","avatar":"https://reqres.in/img/faces/7-image.jpg"}
 ],
 "support":{"url":"https://reqres.in/#support-heading","text":"To keep ReqRes free, contributions towards server costs are appreciated!"}}
