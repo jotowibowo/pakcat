47fc0402e185364d06abb1edc4146b43c2960bf1
jotowibowo <66049601+jotowibowo@users.noreply.github.com>
2023-01-11 10:00:28 +0700

    Add files via upload

--- eb_historical_202301.js
+++ eb_historical_202301.js
@@ -1,4 +1,4 @@
-{"page":1,"per_page":6,"total":12,"total_pages":2,"data":[
+let data = [
 {"processing_date":"2022-11-30","customer_nr":"10003645","nama_debitur":"SINERGI DISTRIBUSI UTAMA PT","DRS":"17","ECL_IDR": 6556017},
 {"processing_date":"2022-11-30","customer_nr":"10062810","nama_debitur":"NEW RATNA MOTOR PT","DRS":"19","ECL_IDR": 1255205509},
 {"processing_date":"2022-11-30","customer_nr":"10117095","nama_debitur":"NEW SUBURTEX PT","DRS":"19","ECL_IDR": 637875654},
@@ -6,6 +6,6 @@
 {"processing_date":"2022-11-30","customer_nr":"10171926","nama_debitur":"NIAGA MITRA MAJU CV","DRS":"16","ECL_IDR": 10507389},
 {"processing_date":"2022-11-30","customer_nr":"10211005","nama_debitur":"TRISAKTI BAHARI MANDIRI PT","DRS":"12","ECL_IDR": 2767102},
 {"processing_date":"2022-11-30","customer_nr":"1021823","nama_debitur":"MOJONG MITRA MAJU PT","DRS":"17","ECL_IDR": 204609388},
-{"processing_date":"2022-11-30","customer_nr":"1023965","nama_debitur":"SUKSES EXPAMET PT","DRS":"19","ECL_IDR": 1147804489}
-],
-"support":{"url":"https://reqres.in/#support-heading","text":"To keep ReqRes free, contributions towards server costs are appreciated!"}}
+{"processing_date":"2022-11-30","customer_nr":"1023965","nama_debitur":"SUKSES EXPAMET PT","DRS":"19","ECL_IDR": 1147804489},
+{"processing_date":"2022-11-30","customer_nr":"9987008","nama_debitur":"MAXIM MASPION`PT","DRS":"19","ECL_IDR":  833072789}
+]
